e4ea1d0d6e7cd54f65449c77bf50be7246f8b794
Albert <AlbStepanyan@gmail.com>
2013-10-30 15:16:09 +0100

    Added Factory Pattern

--- factoryPattern.js
+++ factoryPattern.js
@@ -11,7 +11,49 @@
  */
 (function() {
     
+    /**
+     * The factory pattern is a well-known design pattern used in software engineering to abstract 
+     * away the process of creating specific objects. 
+     * With no way to define classes in ECMAScript
+     * Factory patter encapsulates the creation of object within specific interface
+     */
+     //Creating person Object with Factory patterm
+     /**
+      * @method createPerson()
+      * @param {string} name
+      * @param {int} age
+      * @param {string} job
+      * @return {Object} person
+      */
+    function createPerson(name, age, job) {
+        //creating new Object with Object Constructor
+        var person = new Object();
+        //Defining prperty of person name
+        person.name = name;
+        //Defining property of person age
+        person.age = age;
+        //Defining property of person job
+        person.job = job;
+        //Defining method of person sayName()
+        person.sayName = function() {
+            //returning name property of person
+            return this.name;
+        };
+        //returning {Object} person
+        return person;
+    }
+    //Creating Instances of person Object through factory pattern
+    var person1 = createPerson('Albert', 28, 'Software Developer');
+    var person2 = createPerson('Neo', 29, 'The One');
+    //As a result we see that we simulated the class creation by having two different instances of the same Object
+    //Checking logs
+    console.log(person1.name);//logs Albert
+    console.log(person1.age);//logs 28
+    console.log(person1.job);//logs 'Software Developer'
+    console.log(person2.name);//logs Neo
+    console.log(person2.age);//logs 29
+    console.log(person2.job);//logs The one
     
-    
+       
 })();//END OF FUNCTION
 
