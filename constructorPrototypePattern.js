afd6da1d2169a6c975e71550c0c96dc9c24315fc
Albert <AlbStepanyan@gmail.com>
2013-11-01 18:55:58 +0100

    Added Constructor Prototype Pattern

--- constructorPrototypePattern.js
+++ constructorPrototypePattern.js
@@ -12,9 +12,12 @@
     
     
     /**
-     * Dynamic pattern includes Object creation with Constructo Pattern
+     * The most common way of defining custom types is to combine the constructor and prototype 
+     * patterns. The constructor pattern defines instance properties,
+     * whereas the prototype pattern defines methods and shared properties.
+     * Constructor Prototype pattern includes Object creation with Constructor Pattern
      * Sharing across all instances its methods with Prototype Pattern
-     * But Keeping methods in its constructor not to rewrite values and share for every instance
+     * But Keeping methods in its constructor not to rewrite values and not share for every instance
      */
     function Person(name, age, job) {
         //Defining property name;
@@ -23,6 +26,8 @@
         this.job = job;
         //Defining Property Job
         this.age = age;
+        //Defining Property friends
+        this.friends = []; 
     }
     /**
      * @method sayName
@@ -50,7 +55,14 @@
     console.log(person2.sayName());//Logs Neo
     //Loging Method sayName will show that method sayName is equal on both instances
     console.log(person1.sayName === person2.sayName);//Logs true cause its defined on Prototype of Person and its shared
+    //Pushing one value to the friends property of person2
+    person2.friends.push('Morpheus');
+    person2.friends.push('Trinity');
+    person2.friends.push('Neoba');
+    //Checking logs
+    console.log(person1.friends);//Logs empty array []
+    console.log(person2.friends);//Logs ['Morpheus', 'Trinity', 'Neoba']
     
-});//END OF FUNCTION
+})();//END OF FUNCTION
 
 
